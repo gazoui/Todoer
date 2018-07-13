ad7171e10e971e72b2778610581cd28839c59846
Joshua Agee <joshua-agee@utulsa.edu>
2018-07-13 16:33:03 -0400

    Install Realm pods

---
+++ Todoer/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoer
+//
+//  Created by Joshua Agee on 7/12/18.
+//  Copyright © 2018 Joshua Agee. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
